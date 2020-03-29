dec9f5767b8f3096abab33c12eda21d19616677e
tsavlis <tsavlis.thanos@gmail.com>
2020-03-29 02:30:04 +0200

    posts

--- views/HomeScreen.js
+++ views/HomeScreen.js
@@ -1,34 +1,82 @@
 import React, { Component } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  LayoutAnimation,
-  TouchableOpacity
-} from "react-native";
-import firebase from "firebase";
-
-export default class HomeScreen extends Component {
-  state = {
-    email: "",
-    displayName: ""
-  };
-  componentDidMount() {
-    const { email, displayName } = firebase.auth().currentUser;
-    this.setState({ email, displayName });
+import { Text, View, StyleSheet, FlatList, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import moment from "moment";
+posts = [
+  {
+    id: "1",
+    name: "jon do",
+    text: "lorem10ww",
+    timestamp: 1569492429,
+    avatar: require("../assets/images/profile.jpg"),
+    image: require("../assets/images/profile.jpg")
+  },
+  {
+    id: "2",
+    name: "jon do",
+    text: "lorem10ww",
+    timestamp: 1569492429,
+    avatar: require("../assets/images/profile.jpg"),
+    image: require("../assets/images/profile.jpg")
   }
+];
+export default class HomeScreen extends Component {
+  renderPost = post => {
+    console.log(post);
+    return (
+      <View style={styles.feedItem}>
+        <Image source={post.avatar} style={styles.avatar} />
+        <View style={{ flex: 1 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center"
+            }}
+          >
+            <View>
+              <Text style={styles.name}>{post.name}</Text>
+              <Text style={styles.timestamp}>
+                {moment(post.timestamp).fromNow()}
+              </Text>
+            </View>
 
-  signOutUser = () => {
-    firebase.auth().signOut();
+            <Ionicons name="ios-more" size={24} color="#73788B" />
+          </View>
+
+          <Text style={styles.post}>{post.text}</Text>
+          <Image
+            source={post.image}
+            style={styles.postImage}
+            resizeMode="cover"
+          />
+
+          <View style={{ flexDirection: "row" }}>
+            <Ionicons
+              name="ios-heart-empty"
+              size={24}
+              color="#73788B"
+              style={{ marginHorizontal: 16 }}
+            />
+            <Ionicons name="ios-chatboxes" size={24} color="#73788B" />
+          </View>
+        </View>
+      </View>
+    );
   };
   render() {
-    LayoutAnimation.easeInEaseOut();
     return (
       <View style={styles.container}>
-        <Text>Welcome {this.state.displayName} </Text>
-        <TouchableOpacity style={{ marginTop: 32 }} onPress={this.signOutUser}>
-          <Text>Logout</Text>
-        </TouchableOpacity>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Feed</Text>
+        </View>
+        <FlatList
+          style={styles.feed}
+          data={posts}
+          renderItem={({ item }) => this.renderPost(item)}
+          keyExtractor={item => item.id}
+          showsVerticalScrollIndicator={false}
+        />
       </View>
     );
   }
@@ -37,7 +85,62 @@ export default class HomeScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#EFECF4"
+  },
+  header: {
+    paddingTop: 64,
+    paddingBottom: 16,
+    backgroundColor: "#FFF",
+    alignItems: "center",
     justifyContent: "center",
-    alignItems: "center"
+    borderBottomWidth: 1,
+    borderBottomColor: "#EBECF4",
+    shadowColor: "#454D65",
+    shadowOffset: { height: 5 },
+    shadowRadius: 15,
+    shadowOpacity: 0.5,
+    elevation: 5,
+    zIndex: 10
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: "500"
+  },
+  feed: {
+    marginHorizontal: 16
+  },
+  feedItem: {
+    backgroundColor: "#FFF",
+    borderRadius: 5,
+    padding: 8,
+    flexDirection: "row",
+    marginVertical: 8
+  },
+  avatar: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    marginRight: 16
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#454D65"
+  },
+  timestamp: {
+    fontSize: 11,
+    color: "#C4C6CE",
+    marginTop: 4
+  },
+  post: {
+    marginTop: 16,
+    fontSize: 14,
+    color: "#838899"
+  },
+  postImage: {
+    width: undefined,
+    height: 150,
+    borderRadius: 5,
+    marginVertical: 16
   }
 });
